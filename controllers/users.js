f103feb76035e720207bd383589d0835c507bda0
lenuser <elena-lisina1991@yandex.ru>
2023-09-14 19:53:07 +0700

    v

--- controllers/users.js
+++ controllers/users.js
@@ -30,9 +30,9 @@ module.exports.getUserById = (req, res) => {
         }
         res.send(user);
       })
-      .catch(() => res.status(404).send({ message: 'Пользователь по указанному Id не найден' }));
+      .catch(() => res.status(400).send({ message: 'Некорректный Id' }));
   } else {
-    res.status(400).send({ message: 'Некорректный Id' });
+    res.status(500).send({ message: 'На сервере произошла ошибка' });
   }
 };
 
