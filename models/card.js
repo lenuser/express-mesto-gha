2e484042964233af4bc026eeb41b2db75fc0650e
lenuser <elena-lisina1991@yandex.ru>
2023-09-15 01:20:32 +0700

    k

--- models/card.js
+++ models/card.js
@@ -18,7 +18,7 @@ const cardSchema = new mongoose.Schema({
   },
   likes: [
     {
-      type:[mongoose.Schema.Types.ObjectId],
+      type: [mongoose.Schema.Types.ObjectId],
       ref: 'user',
       default: [],
     },
