d0cfa20ae0dcaaaa8e87a0402376063e5551542b
lenuser <elena-lisina1991@yandex.ru>
2023-09-15 01:43:18 +0700

    k

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router
   .use('/users', users)
-  .use('cards', cards)
+  .use('/cards', cards)
   .use('*', (req, res) => res.status(404).send({ message: 'Страница не найдена' }));
 
 module.exports = router;
