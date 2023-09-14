adf5268047c067ac6fb59e70640adb93646b02b4
lenuser <elena-lisina1991@yandex.ru>
2023-09-15 00:59:55 +0700

    k

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect(DB_URL, {
-  useNewUrlParser: true
+  useNewUrlParser: true,
 });
 
 app.use((req, res, next) => {
