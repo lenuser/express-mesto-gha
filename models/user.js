f6e7a30110e70cf20ed00201be9f9337a26e74ef
lenuser <elena-lisina1991@yandex.ru>
2023-09-18 23:48:56 +0700

    j

--- models/user.js
+++ models/user.js
@@ -4,24 +4,21 @@ const validator = require('validator');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'Поледолжно быть заполнено'],
-    minlength: [2, 'Минимальная длина имени — 2 символа'],
-    maxlength: [30, 'Максимальная — 30 символов'],
+    minlength: 2,
+    maxlength: 30,
     default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    required: [true, 'Поле должно быть заполнено'],
-    minlength: [2, 'Минимальная длина поля — 2 символа'],
-    maxlength: [30, 'Максимальная длина поля — 30 символов'],
+    minlength: 2,
+    maxlength: 30,
     default: 'Исследователь',
   },
   avatar: {
     type: String,
-    required: [true, 'Поледолжно быть заполнено'],
     validate: {
       validator(v) {
-        return /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/.test(v);
+        return /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)/.test(v);
       },
       message: (props) => `${props.value} некорректная ссылка!`,
     },
@@ -40,7 +37,8 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
-    select: false,
+    select: false, // необходимо добавить поле select
   },
-}, { versionKey: false });
+});
+
 module.exports = mongoose.model('user', userSchema);
