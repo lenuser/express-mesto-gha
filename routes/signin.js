0af1c72b1f9cae8eceb7ca96bdda0899350150b2
lenuser <elena-lisina1991@yandex.ru>
2023-09-20 23:39:29 +0700

    kjh

--- routes/signin.js
+++ routes/signin.js
@@ -3,10 +3,10 @@ const { celebrate, Joi } = require('celebrate');
 const { login } = require('../controllers/users');
 
 router.post('/', celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required().min(3),
-    }),
-  }), login);
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(3),
+  }),
+}), login);
 
 module.exports = router;
--- routes/signup.js
+++ routes/signup.js
@@ -4,13 +4,13 @@ const { createUser } = require('../controllers/users');
 const urlRegex = require('../constants/constants');
 
 router.post('/', celebrate({
-    body: Joi.object().keys({
-      name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
-      avatar: Joi.string().pattern(urlRegex),
-      email: Joi.string().required().email(),
-      password: Joi.string().required().min(3),
-    }).unknown(true),
-  }), createUser);
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(urlRegex),
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(3),
+  }).unknown(true),
+}), createUser);
 
 module.exports = router;
